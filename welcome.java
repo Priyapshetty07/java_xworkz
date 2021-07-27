8208199f4a0fe99c996a1e04f3639821d9fd1e5c
Priyapshetty07 <priyapshetty007@gmail.com>
2021-07-27 15:50:25 +0530

    push welcome program

---
+++ welcome.java
@@ -0,0 +1,14 @@
+class welcome{
+
+   public static void main(String args[]){
+
+       System.out.println("welcome" +args[0]);
+       System.out.println("welcome" +args[1]);
+       System.out.println("welcome" +args[2]);
+       System.out.println("welcome" +args[3]);
+       System.out.println("welcome" +args[4]);
+       System.out.println("welcome" +args[5]);
+       System.out.println("welcome" +args[6]);
+
+      }
+}
